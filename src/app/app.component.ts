643d5eb7befad39c6ce9d2915bb9dd49f97799d3
Jaime Alexander Alarcón Alarcón <jalarcon@multiraices.com.ec>
2022-11-01 17:07:38 -0500

    converted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NFC, Ndef } from '@awesome-cordova-plugins/nfc/ngx';
 
 @Component({
@@ -6,13 +6,20 @@ import { NFC, Ndef } from '@awesome-cordova-plugins/nfc/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public ip: string;
   public port: string;
   public requestURL: string;
   readerMode$: any;
+  info: any;
   constructor(private nfc: NFC, private ndef: Ndef) {}
 
+  async ngOnInit() {
+    this.info = {
+      puerta:1,
+      localidades:'Palcos 1'
+    };
+  }
   readCard() {
     // Read NFC Tag - Android
     // Once the reader mode is enabled, any tags that are scanned are sent to the subscriber
@@ -23,7 +30,6 @@ export class AppComponent {
       (err) => console.log('Error reading tag', err)
     );
   }
-
   onNfc(nfcEvent) {
 
     console.log(nfcEvent.tag);
@@ -45,4 +51,47 @@ configurar(){
 
 }
 
+convertHex2Bin(hex: string){
+  let nuevo = '';
+  const hexlen = hex.length;
+  if(hexlen > 23 ){
+    nuevo = (parseInt(hex, 16)).toString(2).substring(-23);
+  }
+  else{
+    nuevo = (parseInt(hex, 16)).toString(2);
+  }
+
+  return nuevo;
+
 }
+convertNumber(n: string, fromBase: number, toBase: number): string {
+  let converted = '';
+  if (fromBase === void 0) {
+    fromBase = 10;
+  }
+  if (toBase === void 0) {
+    toBase = 10;
+  }
+
+  // eslint-disable-next-line eqeqeq
+  if(fromBase === 16 ){
+    if (n.length > 23){
+      converted = parseInt(n.toString(), fromBase).toString(toBase).substring(-23);
+    }
+    else{
+      converted = (parseInt(n, fromBase)).toString(toBase);
+    }
+  }
+  else{
+    converted = (parseInt(n, fromBase)).toString(toBase);
+  }
+
+  return converted;//parseInt(n.toString(), fromBase).toString(toBase);
+}
+
+convBin2Dec(n: string){
+
+}
+}
+
+
