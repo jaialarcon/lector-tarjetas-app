5be4403eb4368fbc656507c635f1f842c09f91e7
Jaime Alexander Alarcón Alarcón <jalarcon@multiraices.com.ec>
2022-11-01 11:00:11 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,48 @@
 import { Component } from '@angular/core';
+import { NFC, Ndef } from '@awesome-cordova-plugins/nfc/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  public ip: string;
+  public port: string;
+  public requestURL: string;
+  readerMode$: any;
+  constructor(private nfc: NFC, private ndef: Ndef) {}
+
+  readCard() {
+    // Read NFC Tag - Android
+    // Once the reader mode is enabled, any tags that are scanned are sent to the subscriber
+    // eslint-disable-next-line no-bitwise
+    const flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V;
+    this.readerMode$ = this.nfc.readerMode(flags).subscribe(
+      (tag) => console.log(JSON.stringify(tag)),
+      (err) => console.log('Error reading tag', err)
+    );
+  }
+
+  onNfc(nfcEvent) {
+
+    console.log(nfcEvent.tag);
+
+    const message = [
+        // eslint-disable-next-line no-new-wrappers
+        this.ndef.textRecord('test')
+    ];
+
+    this.nfc.write(
+        message,
+    );
+
+    this.nfc.addNdefListener(this.onNfc);
+}
+configurar(){
+  //http://localhost:8080/cardService/v1/findByCode/6834452
+  this.requestURL = 'http:'+this.ip+':'+this.port+'cardService/V1';
+
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule  } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [FormsModule ,BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
