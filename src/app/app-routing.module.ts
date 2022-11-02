5575101362864a93d3e9ecad42ba8c8ea49d85f9
Jaime Alarcón <jaiaalar@espol.edu.ec>
2022-11-02 13:33:24 -0500

    funcionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ const routes: Routes = [
   {
     path: '',
     component: AppComponent
-  }
+  },
+  {
+    path: '',
+    loadChildren: () => import('./app.module').then(m => m.AppModule)
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouteReuseStrategy } from '@angular/router';
+import { NFC, Ndef } from '@awesome-cordova-plugins/nfc/ngx';
+
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -11,7 +13,7 @@ import { AppRoutingModule  } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [FormsModule ,BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },NFC,Ndef],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
